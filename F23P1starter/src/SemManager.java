1d20e6ab63acdebfbe5cc05e9f658816a904419e
chwjhevening <chwjhevening@gmail.com>
2023-08-29 22:48:09 -0400

    Added my name, moved honor pledge to line 1

--- F23P1starter/src/SemManager.java
+++ F23P1starter/src/SemManager.java
@@ -1,6 +1,25 @@
+// On my honor:
+// - I have not used source code obtained from another current or
+// former student, or any other unauthorized source, either
+// modified or unmodified.
+//
+// - All source code and documentation used in my program is
+// either my original work, or was derived by me from the
+// source code published in the textbook for this course.
+//
+// - I have not discussed coding details about this project with
+// anyone other than my partner (in the case of a joint
+// submission), instructor, ACM/UPE tutors or the TAs assigned
+// to this course. I understand that I may discuss the concepts
+// of this program with other students, and that another student
+// may help me debug my program so long as neither of us writes
+// anything during the discussion or modifies any computer file
+// during the discussion. I have violated neither the spirit nor
+// letter of this restriction.
 
 /**
- * This program manages a database of seminars whose data is read from a command file.
+ * This program manages a database of seminars whose data is read from a command
+ * file.
  */
 
 import java.io.FileNotFoundException;
@@ -9,45 +28,26 @@ import java.io.FileNotFoundException;
  * The class containing the main method.
  *
  * @author Phillip Jordan
+ * @author Ta-Jung (David) Lin (davidsmile)
  * @version 1.0
  */
-
-// On my honor:
-// - I have not used source code obtained from another current or
-//   former student, or any other unauthorized source, either
-//   modified or unmodified.
-//
-// - All source code and documentation used in my program is
-//   either my original work, or was derived by me from the
-//   source code published in the textbook for this course.
-//
-// - I have not discussed coding details about this project with
-//   anyone other than my partner (in the case of a joint
-//   submission), instructor, ACM/UPE tutors or the TAs assigned
-//   to this course. I understand that I may discuss the concepts
-//   of this program with other students, and that another student
-//   may help me debug my program so long as neither of us writes
-//   anything during the discussion or modifies any computer file
-//   during the discussion. I have violated neither the spirit nor
-//   letter of this restriction.
-
-
 public class SemManager {
     /**
      * @param args
-     *     Command line parameters
+     *            Command line parameters
      */
     public static void main(String[] args) {
         // This is the main file for the program.
-    	int memsize = Integer.parseInt(args[0]);
-    	int hashsize = Integer.parseInt(args[1]);
-    	String fname = args[2];
-    	HashTable db = new HashTable(memsize, hashsize);
-    	try {
-    	SemParser fileRead = new SemParser(fname, db);
-    	} catch (FileNotFoundException e) {
-    	    e.printStackTrace();
-    	}
+        int memsize = Integer.parseInt(args[0]);
+        int hashsize = Integer.parseInt(args[1]);
+        String fname = args[2];
+        HashTable db = new HashTable(memsize, hashsize);
+        try {
+            SemParser fileRead = new SemParser(fname, db);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         Seminar dum = new Seminar();
     }
 }
