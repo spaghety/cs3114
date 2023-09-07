0fd4c429950fb5819dabee4ef8eb9e7c9e55818e
chwjhevening <chwjhevening@gmail.com>
2023-09-06 20:58:13 -0400

    Created test class for HashTable

---
+++ F23P1starter/src/HashTableTest.java
@@ -0,0 +1,19 @@
+import student.TestCase;
+
+/**
+ * 
+ */
+public class HashTableTest {
+    private HashTable hashTable;
+    
+    /**
+     * Sets up the tests that follow.
+     */
+    public void setUp() {
+        hashTable = new HashTable(16, 16);
+    }
+    
+    public void testHashTable() {
+        // TODO getters will be needed for this...
+    }
+}
