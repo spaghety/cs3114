4ce37d9e94e50f117d89926e21558535416e2d66
Phillip <phillip.a.jordan@gmail.com>
2023-09-12 08:41:29 -0400

    Implemented find method in memory manager class

--- F23P1starter/src/MemManager.java
+++ F23P1starter/src/MemManager.java
@@ -84,4 +84,24 @@ public class MemManager {
         freespace[freespace.length - 1] = newblock;
         memory = newBytes;
     }
+    
+    /**
+     * This method finds a seminar in the byte array and returns it in original object form.
+     * @param key the record object referencing the location of the serialized object
+     * @return the deserialized object
+     */
+    public Seminar find(SemRecord key) {
+        if (key == null) {
+            System.out.println("ERROR: hash table record null");
+            return null;
+        }
+        byte[] ser = new byte[key.getSize()];
+        System.arraycopy(memory, key.getIndex(), ser, 0, key.getSize());
+        try {
+            return Seminar.deserialize(ser);
+        } catch (Exception e) {
+            System.out.printf("\nSearch FAILED -- There is no record with ID %d", key.getId());
+            return null;
+        }
+    }
 }
