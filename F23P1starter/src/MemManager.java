88643304773e17206250e336bef490429b230da1
Phillip <phillip.a.jordan@gmail.com>
2023-09-12 08:53:40 -0400

    Implemented Memory Manager remove method. Added javadocs as appropriate

--- F23P1starter/src/MemManager.java
+++ F23P1starter/src/MemManager.java
@@ -84,10 +84,15 @@ public class MemManager {
         freespace[freespace.length - 1] = newblock;
         memory = newBytes;
     }
-    
+
+
     /**
-     * This method finds a seminar in the byte array and returns it in original object form.
-     * @param key the record object referencing the location of the serialized object
+     * This method finds a seminar in the byte array and returns it in original
+     * object form.
+     * 
+     * @param key
+     *            the record object referencing the location of the serialized
+     *            object
      * @return the deserialized object
      */
     public Seminar find(SemRecord key) {
@@ -99,9 +104,43 @@ public class MemManager {
         System.arraycopy(memory, key.getIndex(), ser, 0, key.getSize());
         try {
             return Seminar.deserialize(ser);
-        } catch (Exception e) {
-            System.out.printf("\nSearch FAILED -- There is no record with ID %d", key.getId());
+        }
+        catch (Exception e) {
+            System.out.printf(
+                "\nSearch FAILED -- There is no record with ID %d", key
+                    .getId());
             return null;
         }
+    }
+
+
+    /**
+     * Removes memory from the byte array by allocating it as free space in the
+     * free block.
+     * 
+     * @param key
+     *            object referencing the location of the serialized object being
+     *            removed
+     * @return true if successful, false if not
+     */
+    public boolean remove(SemRecord key) {
+        if (key == null) {
+            System.out.println("ERROR: hash table record null");
+            return false;
+        }
+        int freeBlockIndex = (int)Math.ceil(Math.log(key.getSize()) / Math.log(
+            2));
+        FreeBlock curr = freespace[freeBlockIndex];
+        while (curr != null) {
+            if (curr.getIndex() == key.getIndex()) {
+                System.out.println("ERROR: Record has already been removed");
+                return false;
+            }
+            curr = curr.getNext();
+        }
+        FreeBlock temp = new FreeBlock(key.getIndex());
+        temp.setNext(curr);
+        freespace[freeBlockIndex] = temp;
+        return true;
     }
 }
