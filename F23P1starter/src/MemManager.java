3f35f7cd3a94327071edd230311fc2f4bc295360
Phillip <phillip.a.jordan@gmail.com>
2023-09-12 21:06:44 -0400

    Added pledge to various classes

--- F23P1starter/src/MemManager.java
+++ F23P1starter/src/MemManager.java
@@ -1,8 +1,25 @@
-/**
- * This class contains the main byte array and freeblock array of linked lists
- */
+// On my honor:
+// - I have not used source code obtained from another current or
+// former student, or any other unauthorized source, either
+// modified or unmodified.
+//
+// - All source code and documentation used in my program is
+// either my original work, or was derived by me from the
+// source code published in the textbook for this course.
+//
+// - I have not discussed coding details about this project with
+// anyone other than my partner (in the case of a joint
+// submission), instructor, ACM/UPE tutors or the TAs assigned
+// to this course. I understand that I may discuss the concepts
+// of this program with other students, and that another student
+// may help me debug my program so long as neither of us writes
+// anything during the discussion or modifies any computer file
+// during the discussion. I have violated neither the spirit nor
+// letter of this restriction.
 
 /**
+ * This class contains the main byte array and freeblock array of linked lists
+ * 
  * @author Phillip Jordan (alexj14)
  * @author Ta-Jung (David) Lin (davidsmile)
  * @version 2023.09.11
@@ -155,9 +172,6 @@ public class MemManager {
             }
             curr = curr.getNext();
         }
-        /*System.out.printf(
-            "\nDIAGNOSTIC REMOVE:\nID = %d\nSIZE = %d\nINDEX = %d\n", key
-                .getId(), freeBlockIndex, key.getIndex());*/
         FreeBlock temp = new FreeBlock(key.getIndex());
         temp.setNext(freespace[freeBlockIndex]);
         freespace[freeBlockIndex] = temp;
