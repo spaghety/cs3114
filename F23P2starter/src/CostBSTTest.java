020add2efe42207069c543f4ece4d16ca2599873
Phillip <phillip.a.jordan@gmail.com>
2023-09-24 15:04:56 -0400

    Cost BST Test implemented

--- F23P2starter/src/CostBSTTest.java
+++ F23P2starter/src/CostBSTTest.java
@@ -8,9 +8,54 @@ import student.TestCase;
  * @version 2023.09.24
  */
 public class CostBSTTest extends TestCase {
+    CostBST root;
+    CostBST second;
+    String[] tags;
+    Seminar sem;
+    Seminar secondSem;
 
+    /**
+     * Set up variables for test cases
+     */
     public void setUp() {
-        fail("Not yet implemented");
+        tags = new String[] { "tag1", "tag2", "tag3" };
+        sem = new Seminar(1, "example", "Oct30", 90, (short)2, (short)14, 23,
+            tags, "Test description");
+        root = new CostBST(sem);
+        secondSem = new Seminar(1, "example", "Oct30", 90, (short)2, (short)14,
+            23, tags, "Test description");
+        second = new CostBST(secondSem);
+    }
+
+
+    /**
+     * Test getCost method
+     */
+    public void testGetCost() {
+        assertEquals(23, root.getCost());
+    }
+
+
+    /**
+     * Test getSem method
+     */
+    public void testGetSem() {
+        assertTrue(sem.equals(root.getSem()));
+    }
+
+
+    /**
+     * Test left child setter and getter
+     */
+    public void testSetGetLeft() {
+        root.setLeft(second);
+        assertTrue(secondSem.equals(root.getLeft().getSem()));
+    }
+
+
+    public void testSetGetRight() {
+        root.setRight(second);
+        assertTrue(secondSem.equals(root.getRight().getSem()));
     }
 
 }
