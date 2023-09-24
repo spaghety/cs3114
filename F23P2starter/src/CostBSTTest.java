1ba9fd3c12d14174cc9a8802d1fb753617a2d40e
Phillip <phillip.a.jordan@gmail.com>
2023-09-24 14:05:06 -0400

    Completed DateBSTTest class with javadocs and full coverage

---
+++ F23P2starter/src/CostBSTTest.java
@@ -0,0 +1,16 @@
+import student.TestCase;
+
+/**
+ * Tests the CostBST class
+ * 
+ * @author Phillip Jordan (alexj14)
+ * @author Ta-Jung (David) Lin (davidsmile)
+ * @version 2023.09.24
+ */
+public class CostBSTTest extends TestCase {
+
+    public void setUp() {
+        fail("Not yet implemented");
+    }
+
+}
--- F23P2starter/src/DateBSTTest.java
+++ F23P2starter/src/DateBSTTest.java
@@ -8,9 +8,55 @@ import student.TestCase;
  * @version 2023.09.24
  */
 public class DateBSTTest extends TestCase {
+    Seminar testSem;
+    Seminar testSems;
+    DateBST root;
+    DateBST second;
 
+    /**
+     * Sets up the objects used throughout the test cases
+     */
     public void setUp() {
-        fail("Not yet implemented");
+        String[] tags = new String[] { "tag1", "tag2", "tag3" };
+        testSem = new Seminar(0, "test", "Jan132002", 13, (short)4, (short)9,
+            15, tags, "test description");
+        root = new DateBST(testSem);
+        testSems = new Seminar(1, "second test name", "Jun222010", 90, (short)2,
+            (short)13, 42, tags, "test description two");
+        second = new DateBST(testSems);
     }
 
+
+    /**
+     * Tests the getDate method
+     */
+    public void testGetDate() {
+        assertTrue(root.getDate().equals("Jan132002"));
+    }
+
+
+    /**
+     * Tests the getSem method
+     */
+    public void testGetSem() {
+        assertTrue(root.getSem().equals(testSem));
+    }
+
+
+    /**
+     * Tests the getLeft and setLeft methods
+     */
+    public void testGetSetLeft() {
+        root.setLeft(second);
+        assertTrue(testSems.equals(root.getLeft().getSem()));
+    }
+
+
+    /**
+     * Tests the getRight and setRight methods
+     */
+    public void testGetSetRight() {
+        root.setRight(second);
+        assertTrue(testSems.equals(root.getRight().getSem()));
+    }
 }
