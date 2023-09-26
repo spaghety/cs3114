72500c5d2d888382585623eb551060a383ef167d
chwjhevening <chwjhevening@gmail.com>
2023-09-25 21:22:27 -0400

    I don't see what this is for

--- F23P2starter/src/IOFileTest.java
+++ F23P2starter/src/IOFileTest.java
@@ -1,18 +1,18 @@
-/**
- * 
- */
-
-/**
- * @author Phillip Jordan (alexj14)
- * @author Ta-Jung (David) Lin (davidsmile)
- *
- */
-public class IOFileTest {
-    String infile;
-    public void setUp() {
-        infile = "P2Sample_input.txt";
-    }
-    public  void testRun() {
-        //main();
-    }
-}
+///**
+// * 
+// */
+//
+///**
+// * @author Phillip Jordan (alexj14)
+// * @author Ta-Jung (David) Lin (davidsmile)
+// *
+// */
+//public class IOFileTest {
+//    String infile;
+//    public void setUp() {
+//        infile = "P2Sample_input.txt";
+//    }
+//    public  void testRun() {
+//        //main();
+//    }
+//}
