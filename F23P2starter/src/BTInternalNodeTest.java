053ea3255e5fa07de11f7402e86987620e7bcabc
chwjhevening <chwjhevening@gmail.com>
2023-10-09 00:32:27 -0400

    javadoc, also added assertNull before setters used

--- F23P2starter/src/BTInternalNodeTest.java
+++ F23P2starter/src/BTInternalNodeTest.java
@@ -16,15 +16,25 @@ public class BTInternalNodeTest extends TestCase {
     public void setUp() {
         node = new BTInternalNode();
     }
-    
+
+
+    /**
+     * Tests setLeft and setRight
+     */
     public void testSetChildren() {
         BinTreeNode testNode = new BTInternalNode();
         node.setLeft(testNode);
         testNode = new BTLeafNode();
         node.setRight(testNode);
     }
-    
+
+
+    /**
+     * Tests getLeft and getRight
+     */
     public void testGetChildren() {
+        assertNull(node.left());
+        assertNull(node.right());
         BinTreeNode testInNode = new BTInternalNode();
         BinTreeNode testLeafNode = new BTLeafNode();
         node.setLeft(testInNode);
