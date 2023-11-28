e7127b315afa9982cea1bb10a9d50acca1c15b8e
Phillip <phillip.a.jordan@gmail.com>
2023-11-28 00:22:12 -0500

    passed 3 instructor reference tests. Not sure why it missed easyinsert

--- F23P4starter/src/GraphProjectTest.java
+++ F23P4starter/src/GraphProjectTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
  */
 public class GraphProjectTest extends TestCase {
     GraphProject it = new GraphProject();
+
     // ----------------------------------------------------------
     /**
      * Read contents of a file into a string
@@ -38,7 +39,7 @@ public class GraphProjectTest extends TestCase {
      * This method is simply to get code coverage of the class declaration.
      */
     public void testQInit() {
-        it.main(new String[] { "10", "testinput.txt" });
+        // it.main(new String[] { "10", "testinput.txt" });
     }
 
 
@@ -50,9 +51,22 @@ public class GraphProjectTest extends TestCase {
         PrintStream newOut = new PrintStream(baos);
         PrintStream oldOut = System.out;
         System.setOut(newOut);
-        it.main(new String[] {"10", "unitTestCaseA.txt"});
+        it.main(new String[] { "10", "unitTestCaseA.txt" });
         System.out.flush();
         System.setOut(oldOut);
-        System.out.println("Here: "+baos.toString());
+        assertEquals("|song1| is added to the Artist database.\r\n"
+            + "|artist1| is added to the Song database.\r\n"
+            + "|song2| is added to the Artist database.\r\n"
+            + "|artist2| is added to the Song database.\r\n"
+            + "|song3| is added to the Artist database.\r\n"
+            + "|artist3| is added to the Song database.\r\n"
+            + "|song3| does not exist in the Song database.\r\n"
+            + "|song5| is added to the Artist database.\r\n"
+            + "|artist5| is added to the Song database.\r\n"
+            + "|song6| is added to the Artist database.\r\n"
+            + "Artist hash table size doubled.\r\n"
+            + "|artist6| is added to the Song database.\r\n"
+            + "Song hash table size doubled.\r\n"
+            + "|song7| is added to the Artist database.\n", baos.toString());
     }
 }
