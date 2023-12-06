86718d931551e6071e9d30a9f7619e77e1fe576a
chwjhevening <chwjhevening@gmail.com>
2023-12-06 11:27:42 -0500

    assertEquals

--- F23P4starter/src/HashTest.java
+++ F23P4starter/src/HashTest.java
@@ -21,14 +21,14 @@ public class HashTest extends TestCase {
      *             either a IOException or FileNotFoundException
      */
     public void testSfold() throws Exception {
-        assertTrue(Hash.h("a", 10000) == 97);
-        assertTrue(Hash.h("b", 10000) == 98);
-        assertTrue(Hash.h("aaaa", 10000) == 1873);
-        assertTrue(Hash.h("aaab", 10000) == 9089);
-        assertTrue(Hash.h("baaa", 10000) == 1874);
-        assertTrue(Hash.h("aaaaaaa", 10000) == 3794);
-        assertTrue(Hash.h("Long Lonesome Blues", 10000) == 4635);
-        assertTrue(Hash.h("Long   Lonesome Blues", 10000) == 4159);
-        assertTrue(Hash.h("long Lonesome Blues", 10000) == 4667);
+        assertEquals(Hash.h("a", 10000), 97);
+        assertEquals(Hash.h("b", 10000), 98);
+        assertEquals(Hash.h("aaaa", 10000), 1873);
+        assertEquals(Hash.h("aaab", 10000), 9089);
+        assertEquals(Hash.h("baaa", 10000), 1874);
+        assertEquals(Hash.h("aaaaaaa", 10000), 3794);
+        assertEquals(Hash.h("Long Lonesome Blues", 10000), 4635);
+        assertEquals(Hash.h("Long   Lonesome Blues", 10000), 4159);
+        assertEquals(Hash.h("long Lonesome Blues", 10000), 4667);
     }
 }
